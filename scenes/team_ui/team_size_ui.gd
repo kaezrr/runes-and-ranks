e850ac910402a91bbb635385c6fec3b7dc75b903
Anjishnu Banerjee <inviktor27@gmail.com>
2026-02-15 16:42:56 +0530

    Add traits

---
+++ scenes/team_ui/team_size_ui.gd
@@ -0,0 +1,18 @@
+class_name TeamSizeUI
+extends PanelContainer
+
+@export var player_stats: PlayerStats
+@export var arena_grid: UnitGrid
+
+@onready var unit_count: Label = %UnitCount
+@onready var too_many_units_icon: TextureRect = %TooManyUnitsIcon
+
+func _ready() -> void:
+	player_stats.changed.connect(_update)
+	arena_grid.unit_grid_changed.connect(_update)
+	_update()
+
+func _update() -> void:
+	var units_used := arena_grid.get_all_units().size()
+	unit_count.text = "%s/%s" % [units_used, player_stats.level]
+	too_many_units_icon.visible = units_used > player_stats.level

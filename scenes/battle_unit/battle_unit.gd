f7c2d62ad29f983748032357c2f76098366af3e9
dreamgamer5000 <antonyrejit@gmail.com>
2026-03-05 13:03:11 +0530

    Add battle unit and animation

---
+++ scenes/battle_unit/battle_unit.gd
@@ -0,0 +1,26 @@
+class_name BattleUnit
+extends Area2D
+
+@export var stats: UnitStats: set = set_stats
+
+@onready var skin : PackedSprite2D = $Skin
+@onready var health_bar := $HealthBar
+@onready var mana_bar := $ManaBar
+@onready var tier_icon : TierIcon = $TierIcon
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+func set_stats(value: UnitStats) -> void:
+	stats = value
+	
+	if not stats or not is_instance_valid(TierIcon):
+		return
+		
+	stats = value.duplicate()
+	collision_layer = stats.team + 1
+	
+	skin.texture = UnitStats.TEAM_SPRITESHEET[stats.team]
+	skin.coordinates = stats.skin_coordinates
+	skin.flip_h = stats.team == stats.Team.PLAYER
+	tier_icon.stats = stats
+	
